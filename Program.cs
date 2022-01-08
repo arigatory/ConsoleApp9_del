fbf7a091fe37995a26de657c7bd6da38f281ca9d
Ivan MSI <arigatory@gmail.com>
2022-01-09 02:29:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using System.IO;
+
+namespace xuy
+{
+    class Program
+    {
+
+        static void Main(string[] ages)
+        {
+
+            string text = File.ReadAllText(@"data.txt");
+            WriteLine(text);
+            
+
+        }
+    }
+}
